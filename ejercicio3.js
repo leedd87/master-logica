10fac8ea27eaa9f4f3bb6fe4b7f5f08b2c086050
leedd87 <leedav87@gmail.com>
2023-04-14 14:12:44 -0300

    enunciado ejercicio3

---
+++ ejercicio3.js
@@ -0,0 +1,17 @@
+/*
+Ejercicio 3
+Dada una palabra, buscarla en una frase y devolver cuantas veces aparece en ella
+La frase y la palabra deben ser parametros de una funcion
+
+Ejemplos:
+-Ejecuto esta funcion:
+coincidencias('hola soy una palabra en una frase, PALABRA', 'palabra') // Devuelve 2
+coincidencias('soy la frase','victor') // Devuelve 0
+
+Como hacerlo:
+-Funcion con parametro 'texto'
+-Separar el texto en un array de letras
+-Darle la vuelta
+-Unir el array de letras en un string y comparar con el parametro
+
+*/
