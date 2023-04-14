c81abe78afbcc61f6c3db0861e9742c39b95af11
leedd87 <leedav87@gmail.com>
2023-04-14 17:33:57 -0300

    ejercicio4

---
+++ ejercicio4.js
@@ -0,0 +1,13 @@
+/*
+Ejercicio 4
+Dada una cadena de texto, darle la vuelta e invertir el orden de los caracteres, sin usar metodos propios del lenguaes, solo estructuras de control
+
+
+Ejemplos:
+invertir('hola) // Devuelve: 'aloh'
+invertir('david) // Devuelve: 'divad'
+
+Como hacerlo:
+
+
+*/
