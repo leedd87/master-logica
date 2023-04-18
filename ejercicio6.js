9e0f33958448902b4e200fec7d95d273d6efdc9a
leedd87 <leedav87@gmail.com>
2023-04-18 11:55:34 -0300

    add ejercicio6

---
+++ ejercicio6.js
@@ -0,0 +1,15 @@
+/*
+Ejercicio 6
+Dibujar un cuadrado hueco con asteriscos
+
+
+Ejemplos:
+tantoPorCiento(20,100) // Devuelve: 20
+tantoPorCiento(43,897) // Devuelve: 385.71
+
+Como hacerlo:
+Funcion con parametros 'porcentaje' y 'numero'
+Crear una variable para la operacion del porcentaje
+Devolver string que responda a la pregunta del enunciado
+
+*/
