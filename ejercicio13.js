97fd8c0492653c70561164294d73b0a197a340ee
leedd87 <leedav87@gmail.com>
2023-04-28 10:30:04 -0300

    add ejercicio13.

---
+++ ejercicio13.js
@@ -0,0 +1,17 @@
+/*
+Ejercicio 13
+Dada un array, dividirlo en tantos sub-arrays como sea necesario basandonos en un numero que indique su tamanio
+
+Dividirlo en arrays de X elementos
+
+Ejemplos:
+divideArray([7,8,9,10],2)
+ 
+// Devuelve:
+[[7,8],[9,10]]
+
+Como hacerlo:
+
+*/
+
+let array = [7, 8, 9, 10]
